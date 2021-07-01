01beb65da2ea4cd914a5705e357c3d4c4e53c84f
Tuandeptryz <86765944+Tuandeptryz@users.noreply.github.com>
2021-07-01 14:15:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
+    introTitle: 'Tâm àaaaa 🙆‍♀️',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    btnIntro: 'hehe :v',
+    title: 'Anh biết là em thích anh mà đúng khônggg ?',
+    desc: 'Thử từ chối thử xemm,liệu hồn :v ',
+    btnYes: 'Em thích anhhh :3 ',
+    btnNo: 'Khonggg 🙅‍♀️ ',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại thích anh <3',
+    btnReply: 'Gửi cho Tuann dzz <3',
+    reply: 'Yêu thì yêu mà không yêu cũng phải yêu <33333333',
+    mess: 'Hehe. Anh yeu em ❤️',
+    messDesc: 'Ngủ ngon nhé công chúaaa.',
+    btnAccept: 'Okelaa <3',
+    messLink: 'https://www.facebook.com/tammi0496' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
